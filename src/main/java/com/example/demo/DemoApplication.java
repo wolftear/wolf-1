9d68def8404b83bdc0e6665f5d824208f85a24e2
wolftear <37971209+wolftear@users.noreply.github.com>
2019-08-12 17:24:47 +0800

    Update DemoApplication.java
    feat(外界修改，idea git更新测试)

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
+        System.out.println("界面修改，Idea git更新测试");
         SpringApplication.run(DemoApplication.class, args);
     }
 
