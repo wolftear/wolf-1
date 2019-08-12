99862a2f813b4409741fed02cddf8dc03f494652
zhangzhiyong1 <zhangzhiyong1@yiche.com>
2019-08-12 17:12:10 +0800

    feat(wolf-cloud项目构建)

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
-        System.out.println("git 提交测试");
         SpringApplication.run(DemoApplication.class, args);
     }
 
